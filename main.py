d2fbfe461f127de7d6b55fb707e72a30e062eb8b
98Arvin <arvindoriani@gmail.com>
2024-09-23 13:55:59 +0200

    Triangle able to move and rotate

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -13,10 +15,13 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         # limit to 60 FPS
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
